f4d3c1022cffd8c96de9ea3a43e6fd1b66bcf3aa
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-03-14 19:00:02 +0100

    changes in structure and init()
     first attempt renderBooks() for init()

---
+++ script.js
@@ -0,0 +1,13 @@
+function init(id){
+    renderBooks(id);
+}
+
+function renderBooks(id){
+    let contentRef = document.getElementById(id);
+    contentRef.innerHTML = "";
+    let arr = books;
+    
+    for (let indexBook = 0; indexBook < arr.length; indexBook++) {
+                
+    }
+}
