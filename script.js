c524895b5874f297262c51467218266281ef7f8f
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-03-15 17:13:18 +0100

    like button implemented

--- script.js
+++ script.js
@@ -10,111 +10,99 @@ function renderShelfLevels(id) {
   for (let indexBook = 0; indexBook < arr.length; indexBook++) {
     contentRef.innerHTML += templateShelfLevel(indexBook);
     renderComments(indexBook);
+    toggleLike(indexBook);
   }
 }
 
-// console.log(
-//   books.filter((element) => {return element["name"] == "Die Geheimnisse des Ozeans"})
-// );
-
 function templateShelfLevel(index) {
   return /*html*/ `
-          <div class="shelf-level">
-              <h2>${books[index].name}</h2>
-              <div class="separator"></div>
-              <img src="./assets/img/book-dummy.png" alt="">
-              <div class="separator"></div>
-              <div class="book-info">
-                  <div class="book-info-head">
-                      <span class="book-price">${books[index].price} €</span>
-                      <span class="like-btn">${books[index].likes} ♥️</span>
+      <div class="shelf-level">
+          <h2>${books[index].name}</h2>
+          <div class="separator"></div>
+          <img src="./assets/img/book-dummy.png" alt="${books[index].name} cover">
+          <div class="separator"></div>
+          <div class="book-info">
+              <div class="book-info-head">
+                  <span class="book-price">${books[index].price} €</span>
+                  <div>
+                    <span id="likes${index}"></span>
+                    <button id="like-btn-icon${index}" class="like-btn" onclick="toggleLike(${index})">♥️</button>
                   </div>
-                  <div class="book-info-body">
-                    <div class="key-column">
-                      <span>Author:</span>
-                      <span>Erscheinungsjahr:</span>
-                      <span>Genre</span>
-                    </div>
-                    <div class="value-column">
-                      <span>${books[index].author}</span>
-                      <span>${books[index].publishedYear}</span>
-                      <span>${books[index].genre}</span>
-                    </div>
-                  </div>
-                  <div class="separator"></div>
               </div>
-              <div class="book-comments">
-                  <h3>Kommentare:</h3>   
-                  <div class="book-comments-wrapper">
-                    <div id="book-comments-content${index}" class="books-comments-content-style"></div>
-                  </div>
-                  <div class="book-add-comment">
-                      <input type="text" name="" id="" placeholder="Schreibe dein Kommentar...">
-                      <button>📝</button>
-                  </div>
+              <div class="book-info-body">
+                <div class="key-column">
+                  <span>Author:</span>
+                  <span>Erscheinungsjahr:</span>
+                  <span>Genre</span>
+                </div>
+                <div class="value-column">
+                  <span>${books[index].author}</span>
+                  <span>${books[index].publishedYear}</span>
+                  <span>${books[index].genre}</span>
+                </div>
+              </div>
+              <div class="separator"></div>
+          </div>
+          <div class="book-comments">
+              <h3>Kommentare:</h3>   
+              <div class="book-comments-wrapper">
+                <div id="book-comments-content${index}" class="books-comments-content-table">
+                </div>
+              </div>
+              <div class="book-add-comment">
+                  <input type="text" id="comment-input${index}" class="comment-input-style" placeholder="Schreibe deinen Kommentar...">
+                  <button onclick="addComment(${index})">📝</button>
               </div>
           </div>
-      `;
+      </div>
+  `;
 }
 
-{/* <table>
-<tr>
-    <td>Author:</td>
-    <td>${books[index].author}</td>
-</tr>
-<tr>
-    <td>Erscheinungsjahr:</td>
-    <td>${books[index].publishedYear}</td>
-</tr>
-<tr>
-    <td>Genre</td>
-    <td>${books[index].genre}</td>
-</tr>
-</table> */}
-
-console.log(books[0].comments.length);
+// console.log(books[0].comments.length);
 
 function renderComments(index) {
   let arrComments = books[index].comments;
   let commentsContentRef = document.getElementById("book-comments-content" + index);
   commentsContentRef.innerHTML = "";
 
-switch (arrComments.length) {
-  case 0:
-    commentsContentRef.innerHTML = "keine Kommentare, schreibe du das erste"
-    break;
+  switch (arrComments.length) {
+    case 0:
+      commentsContentRef.innerHTML = "keine Kommentare, schreibe du das erste";
+      break;
 
-  default:
-    for (let indexComments = 0; indexComments < arrComments.length; indexComments++) {
-      let nameAndComment = arrComments[indexComments];
-      commentsContentRef.innerHTML += /*html*/ `
-        <table>
-            <tr>
-                <td>${nameAndComment.name}</td>
-                <td>:${nameAndComment.comment}</td>
-            </tr>
-        </table>
-      `;
-    }
-    break;
+    default:
+      for (let indexComments = 0; indexComments < arrComments.length; indexComments++) {
+        let nameAndComment = arrComments[indexComments];
+        commentsContentRef.innerHTML += templateBookComments(nameAndComment);
+      }
+      break;
+  }
 }
 
-
+function templateBookComments(nameAndComment) {
+  return /*html*/ `
+    <div class="books-comments-content-row">
+      <span>${nameAndComment.name}</span>
+      <span>: ${nameAndComment.comment}</span>
+    </div>
+  `;
 }
 
-{
-  /* <table>
-<tr>
-    <td>Leser123</td>
-    <td>:Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.</td>
-</tr>
-<tr>
-    <td>Leser123</td>
-    <td>:Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.</td>
-</tr>
-<tr>
-    <td>Leser123</td>
-    <td>:Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.</td>
-</tr>
-</table> */
+function addComment(index){
+  inputCommentRef = document.getElementById("comment-input" + index);
+}
+
+function toggleLike(index){
+  let likesRef = document.getElementById(`likes${index}`);
+
+  if (books[index].liked == true) {
+    likesRef.innerHTML = books[index].likes + 1;
+    books[index].liked = false;
+    document.getElementById(`like-btn-icon${index}`).innerHTML = '♥️';
+
+  } else {
+    likesRef.innerHTML = books[index].likes;
+    books[index].liked = true;
+    document.getElementById(`like-btn-icon${index}`).innerHTML = '🖤';
+  }
 }
